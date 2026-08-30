a39db4a431492598bae1d90a3b939d86dfa9797e
pay-to-rank <seohacker@foxmail.com>
2026-08-30 15:44:22 +0800

    Delete api directory

--- api/_alipay.js
+++
@@ -1,9 +0,0 @@
-import AlipaySdk from 'alipay-sdk';
-
-export const alipaySdk = new AlipaySdk({
-  appId: process.env.ALIPAY_APP_ID,
-  privateKey: process.env.ALIPAY_PRIVATE_KEY,        /* 应用私钥 */
-  alipayPublicKey: process.env.ALIPAY_PUBLIC_KEY,    /* 支付宝公钥（注意：不是应用公钥） */
-  signType: 'RSA2',
-  gateway: process.env.ALIPAY_GATEWAY || 'https://openapi.alipay.com/gateway.do',
-});
